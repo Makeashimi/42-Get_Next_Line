84a42f98df96b220295d9591ecef73c59669ed3b
Jennifer CHARLOIS <jcharloi@e2r8p3.42.fr>
2017-01-14 16:46:46 +0100

    GNL

--- main.c
+++ main.c
@@ -12,7 +12,6 @@
 
 #include "Libft/libft.h"
 #include "get_next_line.h"
-#include <stdio.h>
 
 int		main(int argc, char **argv)
 {
@@ -21,23 +20,22 @@ int		main(int argc, char **argv)
 	int		fd2;
 	int		ret2;
 	char	*str1;
-	char 	*str2;
+	char	*str2;
 
 	fd1 = open(argv[1], O_RDONLY);
 	fd2 = open(argv[2], O_RDONLY);
-	while ((ret1 = get_next_line(fd1, &str1)) > 0  || (ret2 = get_next_line(fd2, &str2)) > 0)
+	while ((ret1 = get_next_line(fd1, &str1)) > 0 ||
+		(ret2 = get_next_line(fd2, &str2)) > 0)
 	{
 		ft_putstr("ret1 argv[1] : ");
 		ft_putnbr(ret1);
 		ft_putstr(", line argv[1] : ");
 		ft_putstr(str1);
-
 		ft_putstr(" , ret2 argv[2] : ");
 		ft_putnbr(ret2);
 		ft_putstr(", line argv[2] : ");
 		ft_putstr(str2);
 		ft_putendl("|");
-
 		ft_memdel((void**)&str1);
 		ft_memdel((void**)&str2);
 	}
